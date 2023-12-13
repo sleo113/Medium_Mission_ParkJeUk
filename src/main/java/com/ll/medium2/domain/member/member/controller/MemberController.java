fb62e1b3c0e96f6f46b78cdd806344b5da021132
JeUk Park <qkrwpdnr4531@naver.com>
2023-12-13 13:27:06 +0900

    Modify MemberController

--- src/main/java/com/ll/medium2/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium2/domain/member/member/controller/MemberController.java
@@ -1,7 +1,12 @@
 package com.ll.medium2.domain.member.member.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +15,21 @@ public class MemberController {
     @GetMapping("/")
     public String showJoin() {
         return "domain/member/member/join";
+    }
+
+    @Setter
+    @Getter
+    public static class JoinForm {
+        @NotBlank
+        private String username;
+        @NotBlank
+        private String password;
+    }
+
+    @PostMapping("/join")
+    public String signup(@Valid JoinForm joinForm) {
+
+
+        return "redirect:/";
     }
 }
