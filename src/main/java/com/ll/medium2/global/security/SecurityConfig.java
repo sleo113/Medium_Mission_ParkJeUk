263aa4702bca15f6a8b1433afb5ace5a16a19edf
JeUk Park <qkrwpdnr4531@naver.com>
2023-12-14 17:42:42 +0900

    Step 24

--- src/main/java/com/ll/medium2/global/security/SecurityConfig.java
+++ src/main/java/com/ll/medium2/global/security/SecurityConfig.java
@@ -7,19 +7,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -55,6 +53,11 @@ public class SecurityConfig {
                                         .loginPage("/member/login")
                                         .defaultSuccessUrl("/?msg=" + URLEncoder.encode("환영합니다.", StandardCharsets.UTF_8))
                                         .failureUrl("/member/login?failMsg=" + URLEncoder.encode("아이디 또는 비밀번호가 틀렸습니다.", StandardCharsets.UTF_8))
+                )
+                .logout(
+                        logout ->
+                                logout
+                                        .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
                 );
 
         return http.build();
