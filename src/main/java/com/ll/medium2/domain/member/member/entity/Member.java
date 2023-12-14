e648c81e9ca8645d2752c5b80e127725ea3255ba
JeUk Park <qkrwpdnr4531@naver.com>
2023-12-14 17:21:53 +0900

    Step 22-2

--- src/main/java/com/ll/medium2/domain/member/member/entity/Member.java
+++ src/main/java/com/ll/medium2/domain/member/member/entity/Member.java
@@ -8,8 +8,13 @@ import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 import static lombok.AccessLevel.PROTECTED;
@@ -35,5 +40,17 @@ public class Member {
     public Member(String username, String password) {
         this.username = username;
         this.password = password;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority("ROLE_MEMBER"));
+
+        if (List.of("system", "admin").contains(username)) {
+            authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
+        }
+
+        return authorities;
     }
 }
