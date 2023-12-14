2ad4d43723a67cba53027b61ca9bece61ecaf201
JeUk Park <qkrwpdnr4531@naver.com>
2023-12-14 17:09:38 +0900

    Step 17-4

--- src/main/java/com/ll/medium2/global/init/NotProd.java
+++ src/main/java/com/ll/medium2/global/init/NotProd.java
@@ -1,17 +1,26 @@
 package com.ll.medium2.global.init;
 
+import com.ll.medium2.domain.member.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
 public class NotProd {
+
+    private final MemberService memberService;
+
     @Bean
     public ApplicationRunner initNotProd() {
-        return args -> log.debug("NotProd 실행됨");
+        return args -> {
+            memberService.join("system", "system");
+            memberService.join("admin", "admin");
+            memberService.join("user1", "1234");
+            memberService.join("user2", "1234");
         };
-    }
+    };
 }
