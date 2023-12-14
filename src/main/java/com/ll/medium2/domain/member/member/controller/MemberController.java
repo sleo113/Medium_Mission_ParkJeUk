dad454903df5d4967a56b6fd0a002093f783f56a
JeUk Park <qkrwpdnr4531@naver.com>
2023-12-14 17:16:27 +0900

    Step 20

--- src/main/java/com/ll/medium2/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium2/domain/member/member/controller/MemberController.java
@@ -7,7 +7,9 @@ import com.ll.medium2.global.rsData.RsData.RsData;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,20 +17,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
     private final Rq rq;
 
-    public MemberController(MemberService memberService, Rq rq) {
-        this.memberService = memberService;
-        this.rq = rq;
-    }
-
-    @GetMapping("/")
+    @PreAuthorize("isAnonymous()")
+    @GetMapping("/join")
     public String showJoin() {
         return "domain/member/member/join";
     }
-
     @Setter
     @Getter
     public static class JoinForm {
@@ -38,14 +36,12 @@ public class MemberController {
         private String password;
     }
 
+    @PreAuthorize("isAnonymous()")
     @PostMapping("/join")
     public String join(@Valid JoinForm joinForm) {
         RsData<Member> joinRs = memberService.join(joinForm.getUsername(), joinForm.getPassword());
-
-
         return rq.redirectOrBack(joinRs, "/member/login");
     }
-
     @GetMapping("/login")
     public String showLogin() {
         return "domain/member/member/login";
