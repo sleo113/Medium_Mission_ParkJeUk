8cdd8664525a35095395684f19b3d74b6c73d172
JeUk Park <qkrwpdnr4531@naver.com>
2023-12-14 15:57:29 +0900

    Add Rq

--- src/main/java/com/ll/medium2/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium2/domain/member/member/controller/MemberController.java
@@ -2,15 +2,16 @@ package com.ll.medium2.domain.member.member.controller;
 
 import com.ll.medium2.domain.member.member.entity.Member;
 import com.ll.medium2.domain.member.member.service.MemberService;
+import com.ll.medium2.global.rq.Rq.Rq;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.catalina.util.URLEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.apache.catalina.util.URLEncoder;
 
 import java.nio.charset.StandardCharsets;
 
@@ -18,9 +19,11 @@ import java.nio.charset.StandardCharsets;
 @RequestMapping("/member")
 public class MemberController {
     private final MemberService memberService;
+    private final Rq rq;
 
-    public MemberController(MemberService memberService) {
+    public MemberController(MemberService memberService, Rq rq) {
         this.memberService = memberService;
+        this.rq = rq;
     }
 
     @GetMapping("/")
