711ceac748c9fca9ad76930c5d5dd545d928ea93
JeUk Park <qkrwpdnr4531@naver.com>
2023-12-14 17:52:12 +0900

    Step 26

---
+++ src/main/java/com/ll/medium2/domain/post/post/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package com.ll.medium2.domain.post.post.repository;
+
+import com.ll.medium2.domain.post.post.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
